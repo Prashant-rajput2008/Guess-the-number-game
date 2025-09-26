8687f327ee5e5607f606bad2d9e49f987e5c289f
Prashant Rajput <prashantkumarrajput582008@gmail.com>
2025-09-26 20:22:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -23,60 +23,50 @@ btn.addEventListener("click", () => {
 
     let inputNumber = input.value
     if(inputNumber > 150 || inputNumber < 0){
-        result1.innerText = "Chori Maanja "
-        result2.innerText = "Sahi te khel le Range Dekh Le"
+        result1.innerText = "Please choose in Between The Range"
 
     }else if(inputNumber < randomNumber && count == 2){
-        result1.innerText = "Yo bhi Chota se"
-        result2.innerText = "Bada Number daal "
+        result1.innerText = "This is Also a Smaller number"
         result1.style.color = "crimson"; 
-        result2.style.color = "crimson"; 
         Guesses += 1
         console.log(count);
 
     }else if(inputNumber < randomNumber){
-        result1.innerText = "Yo to Chota se"
-        result2.innerText = "Bada Number daal "
+        result1.innerText = "Please Choose a Larger Number"
         result1.style.color = "crimson"; 
-        result2.style.color = "crimson"; 
         Guesses += 1
         count = 2
         console.log(count);
         
 
     }else if(inputNumber > randomNumber && count == 1){
-        result1.innerText = "Yo bhi Bda se"
-        result2.innerText = "Bada Number daal "
+        result1.innerText = "This is Also a Large number"
         result1.style.color = "orange"; 
-        result2.style.color = "orange"; 
         Guesses += 1
         console.log(count);
 
     }else if(inputNumber > randomNumber){
-        result1.innerText = "Yo to Bada se"
-        result2.innerText = "Chota Number daal"
+        result1.innerText = "Please Choose a Smaller Number"
         result1.style.color = "orange"; 
-        result2.style.color = "orange"; 
         Guesses += 1
         count = 1
         console.log(count);
 
     }else if(inputNumber = randomNumber){
-        result1.innerText = "Mann gya tujhe ib tune sahi jawab diya"
-        result3.innerText = `kul Andaje = ${Guesses}`
-        result2.innerText = ""
+        result1.innerText = "You Guessesed Right "
+        result3.innerText = `Total Guesses = ${Guesses}`
+
         result1.style.color = "limegreen"; 
-        result2.style.color = "limegreen"; 
-        input.placeholder = "Aree yrr Shit......."
+        input.placeholder = "You Win......"
 
         setTimeout(() => {
-            input.placeholder = "It's Not Easy Taniya Dii"
+            input.placeholder = ""
             resetGame();
         }, 5000);
 
     }else{
-        result1.innerText = "Error aa gya Bta mhne Kaise aaya yo error"
-        result2.innerText = ""
+        result1.innerText = "There is a Error"
+
         
     }
 
